87075d4cdba7007e48299dfa18e6226af45305bf
jaceisom <59097831+jaceisom@users.noreply.github.com>
2022-03-16 08:47:24 -0600

    Update receive encrypted page and display on oled.ino

--- receive encrypted page and display on oled.ino
+++ receive encrypted page and display on oled.ino
@@ -34,6 +34,7 @@ const int DELETE_BUTTON = 4;
 int lastStateDelete = HIGH;
 int currentStateDelete;
 int numMessages = 0;
+const int BUZZER = 7;
 
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -77,6 +78,13 @@ void checkReceiverForMessage(){
         String input = String(charData);
         storeMessage(input);
         currMessageIndex = (numMessages - 1);
+        digitalWrite(BUZZER, HIGH);
+        delay(100);
+        digitalWrite(BUZZER, LOW);
+        delay(100);
+        digitalWrite(BUZZER, HIGH);
+        delay(100);
+        digitalWrite(BUZZER, LOW);
         printMessage();
       }
     }
@@ -106,6 +114,11 @@ void printSerialMessages(){
 }
 void printMessage(){
   if (numMessages > 0){
+    String currMessage = String(currMessageIndex + 1) + "/" + String(numMessages);
+    printText(currMessage);
+    delay(250);
+    display.clearDisplay();
+    display.display(); 
     printText(messages.at(currMessageIndex));
   }
   else {
@@ -126,15 +139,21 @@ void previousMessage(){
   printMessage();
 }
 void deleteMessage(){
-  messages.erase(messages.begin() + currMessageIndex);
-  numMessages--;
   if (numMessages > 0){
+    messages.erase(messages.begin() + currMessageIndex);
+    numMessages--;
+    printText("Message\nDeleted");
+    delay(250);
+    display.clearDisplay();
+    display.display(); 
     currMessageIndex = (numMessages - 1);
     printMessage();
   }
-  else{
+  if (numMessages == 0){
+    printText("Inbox\nEmpty");
+    delay(2000);
     display.clearDisplay();
-    currMessageIndex = 0;
+    display.display(); 
   }
 }
 
@@ -148,9 +167,8 @@ void setUpDisplay() {
   display.setTextSize(FONT_SIZE);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
-  display.println("Display Ready");
-  display.display(); 
   display.clearDisplay();
+  display.display(); 
 }
 
 void setup(){
@@ -161,25 +179,30 @@ void setup(){
     pinMode(NEXT_BUTTON, INPUT_PULLUP);
     pinMode(BACK_BUTTON, INPUT_PULLUP);
     pinMode(DELETE_BUTTON, INPUT_PULLUP);
+    pinMode(BUZZER, OUTPUT);
+    digitalWrite(BUZZER, LOW);
 }
 
 void loop(){
   checkReceiverForMessage();
   delay(20);
-  currentStateNext = digitalRead(NEXT_BUTTON);
-  currentStateBack = digitalRead(BACK_BUTTON);
-  currentStateDelete = digitalRead(DELETE_BUTTON);
 
-  if(lastStateNext == LOW && currentStateNext == HIGH){
-    nextMessage();
+  if (numMessages > 0){
+    currentStateNext = digitalRead(NEXT_BUTTON);
+    currentStateBack = digitalRead(BACK_BUTTON);
+    currentStateDelete = digitalRead(DELETE_BUTTON);
+
+    if(lastStateNext == LOW && currentStateNext == HIGH){
+      nextMessage();
+    }
+    if(lastStateBack == LOW && currentStateBack == HIGH){
+      previousMessage();
+    }
+    if(lastStateDelete == LOW && currentStateDelete == HIGH){
+      deleteMessage();
+    }
+    lastStateNext = currentStateNext;
+    lastStateBack = currentStateBack;
+    lastStateDelete = currentStateDelete;
   }
-  if(lastStateBack == LOW && currentStateBack == HIGH){
-    previousMessage();
-  }
-   if(lastStateDelete == LOW && currentStateDelete == HIGH){
-     deleteMessage();
-  }
-  lastStateNext = currentStateNext;
-  lastStateBack = currentStateBack;
-  lastStateDelete = currentStateDelete;
 }
